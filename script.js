e41a8ffe649f2f157f7b6ea65cb0376a5be9385b
Nicole J. C. Lemes <nicolejdecarvalho@gmail.com>
2022-02-10 01:42:15 -0300

    layout mais completo

---
+++ script.js
@@ -0,0 +1,7 @@
+function showSidebar() {
+    const sidebar = document.querySelector("aside");
+    sidebar.classList.remove("hidden");
+    const section = document.querySelector("section");
+    section.classList.add("opacity");
+}
+

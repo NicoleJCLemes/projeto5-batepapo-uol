09cd68232345f1c6506f725f15f15a7172629320
Nicole J. C. Lemes <nicolejdecarvalho@gmail.com>
2022-02-14 02:55:52 -0300

    requisito envio de mensagem e bônus buscar participantes pronto

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let personName = {
     name: answer
 }
 let arrayMsg = [];
+let input = "";
 
 function statusOnOff() {
     const promise = axios.post("https://mock-api.driven.com.br/api/v4/uol/status", personName);
@@ -41,24 +42,26 @@ const message = axios.get("https://mock-api.driven.com.br/api/v4/uol/messages");
 message.then(takeMessages);
 
 function takeMessages(object) {
+    const insert = document.querySelector("section main");
+    insert.innerHTML = "";
     arrayMsg = object.data;
     arrayMsg.forEach(showMessages);
 }
 
 function showMessages(msg){
     const insert = document.querySelector("section main");
-
+    
     if(msg.type === "status") {
         insert.innerHTML = insert.innerHTML + `
         <p class="status"><time>(${msg.time})</time> <b>${msg.from}</b>: ${msg.text}</p>
         `
     } else if ((msg.type === "private_message") && ((msg.from === answer) || (msg.to === answer))){
         insert.innerHTML = insert.innerHTML + `
-        <p class="private"><time>(${msg.time})</time> <b>${msg.from}</b> para <b>${msg.to}</b>: ${msg.text}</p>
+        <p class="private"><time>(${msg.time})</time> <b>${msg.from}</b> reservadamente para <b>${msg.to}</b>: ${msg.text}</p>
         `
     } else {
         insert.innerHTML = insert.innerHTML + `
-        <p><time>(${msg.time})</time> <b>${msg.from}</b> para <b>${msg.to}</b>: ${msg.text}</p>
+        <p class="normal"><time>(${msg.time})</time> <b>${msg.from}</b> para <b>${msg.to}</b>: ${msg.text}</p>
         `
     }
 
@@ -71,12 +74,8 @@ function reloadMessages(){
     reload.then(takeMessages);
 }
 
-/*function eraseMessages(){
-    const insert = document.querySelector("section main");
-    insert.innerHTML = "";
-}*/
-
 setInterval(reloadMessages,3000);
+setInterval(reloadPeople,3000);
 
 function showSidebar() {
     const sidebar = document.querySelector("aside");
@@ -99,4 +98,51 @@ function hideSidebar() {
     unchecked.classList.remove("hidden");
 }*/
 
+const people = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
+people.then(takeParticipants);
 
+function takeParticipants(participants){
+    const insert = document.querySelector("aside menu");
+    insert.innerHTML = "";
+    arrayPeople = participants.data;
+    arrayPeople.forEach(showParticipants)
+}
+
+function showParticipants(participants){
+    const insert = document.querySelector("aside menu");
+    insert.innerHTML = insert.innerHTML + `
+        <div>
+            <ion-icon name="person-circle-sharp"></ion-icon>
+            <p>${participants.name}</p>
+            <ion-icon class="checkmark hidden" name="checkmark-sharp"></ion-icon>
+        </div>
+    `
+}
+
+function reloadPeople(){
+    const reload = axios.get("https://mock-api.driven.com.br/api/v4/uol/participants");
+    reload.then(takeParticipants);
+}
+
+function sendMessage(){
+    input = document.querySelector("footer input").value;
+    let inputMsg = {
+        from: answer,
+        to: "Todos",
+        text: input,
+        type: "message"
+    }
+
+    const send = axios.post("https://mock-api.driven.com.br/api/v4/uol/messages", inputMsg);
+    send.then(reloadMessages);
+    send.catch(reloadPage);
+    eraseInputValue();
+}
+
+function eraseInputValue(){
+    document.querySelector("footer input").value = "";
+}
+
+function reloadPage(error){
+    window.location.reload(true);
+}
